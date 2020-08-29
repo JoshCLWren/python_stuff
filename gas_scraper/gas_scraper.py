8e1c4180cdc276190bcadea63c7fece21e1bb110
Joshisplutar <joshisplutar@gmail.com>
2020-08-29 11:04:39 -0500

    Clean up code

--- gas_scraper/gas_scraper.py
+++ gas_scraper/gas_scraper.py
@@ -21,19 +21,30 @@ def add_gas_price(city):
   data = (gas_price, current_time, city)
   print(f"Gas price for {city} is currently {gas_price}")
   add_to_db(data)
+  print(f"Historic gas prices for {city}:")
+  query = 'SELECT * FROM national_gas_prices WHERE CITY=?'
+  c.execute(query, [city])
 
 
 def add_to_db(data):
   c.execute("INSERT INTO national_gas_prices VALUES (?,?,?)", data)
   connection.commit()
 
+def show_prices(city):
+  print(f"Gas prices for {city}:")
+  query = 'SELECT * FROM national_gas_prices WHERE CITY=?'
+  c.execute(query, [city])
+
 def gas_adder():
   for city in cities:
     connection = sqlite3.connect("gas_prices.db")
-    c = connection.cursor()
+    # c = connection.cursor()
     # c.execute('''CREATE TABLE national_gas_prices (price REAL, dateAdded TEXT, city TEXT );''')
     add_gas_price(city)
     print(f"Adding gas price for {city}.")
+    # show_prices(city)
     connection.close()
 
 gas_adder()
+
+
